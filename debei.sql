e645db56c0d7f10c675100ad49f92515425fc69a
SirJoeth3rd <njdewaal@gmail.com>
2024-12-16 13:55:03 +0200

    cleanup and update in database

--- debei.sql
+++ debei.sql
@@ -5,6 +5,16 @@ CREATE TABLE Items (
   price INTEGER
 );
 
+CREATE TABLE ItemColors (
+  item_nbr INTEGER NOT NULL REFERENCES Items(item_nbr),
+  color_nbr INTEGER NOT NULL REFERENCES Colors(color_nbr)
+);
+CREATE TABLE Colors (
+  color_nbr INTEGER PRIMARY KEY,
+  name TEXT,
+  price_modifier INTEGER DEFAULT 0
+);
+
 CREATE TABLE Orders (
   order_nbr INTEGER PRIMARY KEY,
   name TEXT NOT NULL,
@@ -19,16 +29,21 @@ CREATE TABLE OrderItems (
   order_item_nbr INTEGER PRIMARY KEY,
   item_nbr INTEGER REFERENCES Items(item_nbr),
   order_nbr INTEGER REFERENCES Orders(order_nbr),
-  item_size TEXT CHECK(item_size IN ('S','M','L'))
+  item_size TEXT CHECK(item_size IN ('S','M','L')),
+  item_color INTEGER NOT NULL REFERENCES Colors(color_nbr)
 );
 
 -- Populate Items
 
+INSERT INTO Colors (name, price_modifier)
+  VALUES
+    ('blue', 0),('pink', 0),('zebra', 0);
+
 INSERT INTO Items (description, name, price)
   VALUES
-    ('a lover darkly', 'dress-1', 50000),
-    ('the killer', 'dress-2', 80000),
-    ('a tall glass of water', 'dress-3', 30000),
-    ('mumbo jumbo', 'dress-4', 10000),
-    ('a classy beetle', 'dress-5', 70000),
-    ('the modest mirror', 'dress-6', 35000);
+  ('cheeky in the summer sun', 'tanga', 30000),
+  ('twins of the gazelle','bikini', 20000),
+  ('elegant lady, dancing in the water','slip',30000),
+  ('a waterfall of shimmering skin','triangle_top', 30000),
+  ('beauty in the eyes of all beholders','one_piece', 60000),
+  ('a rising crescent moon', 'one_shoulder_top', 40000);
